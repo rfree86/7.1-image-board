b2418156edd89c0a43daf813ff1a2d2699a038c1
Richard <rfree86@gmail.com>
2015-11-30 16:54:28 -0500

    Update 2015-11-30T21:54:19.356Z

--- app/scripts/models/create.js
+++
@@ -1,9 +0,0 @@
-var Post = Backbone.Model.extend({
-  idAttribute: "_id",
-  defaults: {
-    image: "",
-    caption: "",
-    isEditing: false,
-  }
-});
-export default Post;
--- app/scripts/models/post_collection.js
+++
@@ -1,7 +0,0 @@
-import Post from 'models/create';
-
-var PostsCollection = Backbone.Collection.extend({
-  model: Post,
-  url: "http://tiny-lasagna-server.herokuapp.com/collections/richard_imagepost"
-});
-export default PostsCollection;
